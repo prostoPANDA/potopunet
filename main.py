aa7e164ea0c9c7ec09abfdf00c41941e0ccf32d5
PANDA <95334270+prostoPANDA@users.noreply.github.com>
2023-07-16 15:26:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from bot import send_msg
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('main.html')
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        send_msg(str(name))
+        send_msg(str(phone))
+        return redirect('/success')
+    else:
+        return render_template('form.html')
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(debug=False)
